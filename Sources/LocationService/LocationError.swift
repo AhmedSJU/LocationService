489eced818045d0b521c718008ee43e0d8e483a4
Sheikh Ahmed <sheikh@jerin.app>
2022-05-28 08:12:34 +0100

    added location service funcionalities

---
+++ Sources/LocationService/LocationError.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sheikh Ahmed on 28/05/2022.
+//
+
+import Foundation
+enum LocationError: Error {
+    case unauthorized
+    case unableToDetermineLocation
+}
--- Sources/LocationService/LocationService.swift
+++ Sources/LocationService/LocationService.swift
@@ -1,6 +1,88 @@
-public struct LocationService {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import CoreLocation
+import Combine
 
-    public init() {
+public class LocationService: NSObject {
+    private let locationManager = CLLocationManager()
+    
+    typealias AuthorizationRequest = Result<Void, LocationError>
+    typealias LocationRequest = Result<CLLocation, LocationError>
+    
+    private var authorizationRequests: [(AuthorizationRequest) -> Void] = []
+    private var locationRequests: [(LocationRequest) -> Void] = []
+    
+    var objectWillChange = PassthroughSubject<Void, Never>()
+    var degrees: Double = .zero {
+        didSet {
+            objectWillChange.send()
+        }
+    }
+    
+    public override init() {
+        super.init()
+        locationManager.delegate = self
+        if CLLocationManager.headingAvailable() {
+            locationManager.startUpdatingHeading()
+        }
+    }
+
+    func requestWhenUseAuthorization() -> Future<Void, LocationError> {
+        guard locationManager.authorizationStatus == .notDetermined else {
+            return Future { $0(.success(())) }
+        }
+        let future = Future<Void, LocationError> { completion in
+            self.authorizationRequests.append(completion)
+        }
+        locationManager.requestWhenInUseAuthorization()
+        return future
+    }
+    
+    func requestLocation() -> Future<CLLocation, LocationError> {
+        guard locationManager.authorizationStatus == .authorizedWhenInUse
+                || locationManager.authorizationStatus == .authorizedAlways else {
+                    return Future { $0(.failure(.unauthorized))}
+                }
+        let future = Future<CLLocation, LocationError> { completion in
+            self.locationRequests.append(completion)
+        }
+        locationManager.requestLocation()
+        return future
+    }
+    
+    private func handleLocationRequestResult(_ result: LocationRequest) {
+        while locationRequests.count > 0 {
+            let request = locationRequests.removeFirst()
+            request(result)
+        }
+    }
+}
+
+extension LocationService: CLLocationManagerDelegate {
+    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        let locationError: LocationError
+        if let error = error as? CLError, error.code == .denied {
+            locationError = .unauthorized
+        }
+        else {
+            locationError = .unableToDetermineLocation
+        }
+        handleLocationRequestResult(.failure(locationError))
+    }
+    
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            handleLocationRequestResult(.success(location))
+        }
+    }
+    
+    public func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        while authorizationRequests.count > 0 {
+            let request = authorizationRequests.removeFirst()
+            request(.success(()))
+        }
+    }
+
+    public func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        self.degrees = 1 * newHeading.magneticHeading
     }
 }
